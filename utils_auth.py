b8ec44e632898413088c280c38fa5a513ee9bc40
Pixiepinks <info@pixiepinks.shop>
2025-08-31 19:56:45 +0530

    Create utils_auth.py

---
+++ utils_auth.py
@@ -0,0 +1,8 @@
+# utils_auth.py
+from passlib.hash import bcrypt
+
+def hash_password(raw: str) -> str:
+    return bcrypt.hash(raw)
+
+def verify_password(raw: str, hashed: str) -> bool:
+    return bcrypt.verify(raw, hashed)
